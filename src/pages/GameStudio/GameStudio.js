85a771c3244faf0819a60eca8cda58ee4b4ac1ff
spycouco-study <spycouco@gmail.com>
2025-12-01 17:27:24 +0900

    '데이터'탭에 assets항목은 숨기도록 수정

--- src/pages/GameStudio/GameStudio.js
+++ src/pages/GameStudio/GameStudio.js
@@ -253,7 +253,7 @@ const GameStudio = () => {
                   onDataChange={setGameData}
                   gameName={gameTitle}
                   showImportExport={false}
-                  hiddenTopLevelKeys={['assets']}
+                  hiddenTopLevelKeys={["assets"]}
                 />
               </div>
             )}
